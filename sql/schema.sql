645de0d8aa548e2e1d7d0fdb72f9dd36a73e3798
Naoki Shimizu <naoki.shimizu@dena.com>
2015-09-22 02:29:30 +0900

    add index on login_log table

--- sql/schema.sql
+++ sql/schema.sql
@@ -15,3 +15,6 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `ip` varchar(255) NOT NULL,
   `succeeded` tinyint NOT NULL
 ) DEFAULT CHARSET=utf8;
+
+CREATE INDEX ON `login_log` (`user_id`);
+CREATE INDEX ON `login_log` (`ip`);
